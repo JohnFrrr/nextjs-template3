dc0c48f6d65162ec19746cd844cde568339f6e42
João Ferreira <joaov.ssff@gmail.com>
2023-07-05 21:26:46 -0300

    i

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,30 @@
-import '../styles/globals.css'
+import './globals.css'
+import { Inter } from 'next/font/google'
+import Navbar from './components/Navbar'
+import AuthProvider from './context/AuthProvider'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'NextAuth Tutorial',
+  description: 'Learn NextAuth.js by Dave Gray',
+}
 
 export default function RootLayout({
-    // Layouts must accept a children prop.
-    // This will be populated with nested layouts or pages
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    )
-  }
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AuthProvider>
+          <Navbar />
+          <main className="flex justify-center items-start p-6 min-h-screen">
+            {children}
+          </main>
+        </AuthProvider>
+      </body>
+    </html>
+  )
+}
